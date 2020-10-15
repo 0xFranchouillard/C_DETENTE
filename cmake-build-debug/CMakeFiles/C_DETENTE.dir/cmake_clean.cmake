b37abf814a8cc912dff100c490449aa40fa19d7a
CyrilleChampion <cyrillechampion13@gmail.com>
2020-10-15 09:47:58 +0200

    Hello world
    Hello world

---
+++ cmake-build-debug/CMakeFiles/C_DETENTE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_DETENTE.dir/main.c.obj"
+  "C_DETENTE.exe"
+  "C_DETENTE.exe.manifest"
+  "C_DETENTE.pdb"
+  "libC_DETENTE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_DETENTE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
